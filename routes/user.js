2a2de1eb68a52b006e4be05588a33f02d1093146
Andres Morales <andmoraho@gmail.com>
2018-10-18 11:07:22 -0500

    User /register, /login, /logout, /me routes added. Authenticate middleware added to /logout and /me routes.

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const UserController = require('../controllers/user');
 
+var { authenticate } = require('../middleware/authenticate');
+
 var api = express.Router();
 
 api.get('/home', UserController.home);
 api.get('/tests', UserController.tests);
+api.post('/register', UserController.saveUser);
+api.post('/login', UserController.loginUser);
+api.delete('/logout', authenticate, UserController.logoutUser);
+api.get('/me', authenticate, UserController.getUser);
 
 
 module.exports = api;
