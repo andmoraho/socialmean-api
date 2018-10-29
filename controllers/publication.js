08f3cde118228ca652ec96bf754a9502b66d68b0
Andres Morales <andmoraho@gmail.com>
2018-10-29 13:48:17 -0500

    Added GET /publications/:page? route

--- controllers/publication.js
+++ controllers/publication.js
@@ -9,6 +9,7 @@ var { Publication } = require('../models/publication');
 var { User } = require('../models/user');
 var { Follow } = require('../models/follow');
 
+// POST /publication (authenticated)
 var savePublication = async(req, res) => {
     try {
         const body = _.pick(req.body, ['text', 'file']);
@@ -40,6 +41,57 @@ var savePublication = async(req, res) => {
 
 };
 
+//GET /publications (authenticated)
+var getPublications = async(req, res) => {
+    try {
+        var page = req.params.page || 1;
+        var itemsPerPage = 5;
+        var followsClean = [];
+
+        var followsFiltered = await Follow.find({
+                _user: req.user._id
+            })
+            .populate({
+                path: '_followed',
+                select: '-password -tokens -__v'
+            });
+
+        followsFiltered.forEach((follow) => {
+            followsClean.push(follow._followed);
+        });
+
+        var totalFollowing = await Follow.find({
+            _user: { "$in": followsClean }
+        });
+
+        var publicationsPaginated = await Publication.find({
+                _user: { "$in": followsClean }
+            })
+            .sort('-createdAt')
+            .populate({
+                path: '_user',
+                select: '-password -tokens -__v'
+            })
+            .select({ '__v': 0 })
+            .skip((itemsPerPage * page) - itemsPerPage)
+            .limit(itemsPerPage);
+
+        res.status(200).send({
+            publicationsPaginated,
+            total: totalFollowing.length,
+            pages: Math.ceil(totalFollowing.length / itemsPerPage),
+            currentPage: page
+        });
+
+
+    } catch (error) {
+        res.status(400).send({
+            message: error.message
+        });
+    }
+};
+
 module.exports = {
-    savePublication
+    savePublication,
+    getPublications
 };
--- routes/publication.js
+++ routes/publication.js
@@ -7,5 +7,6 @@ const { md_upload_publications } = require('../middleware/uploadImage');
 var api = express.Router();
 
 api.post('/publication', authenticate, PublicationController.savePublication);
+api.get('/publications/:page?', authenticate, PublicationController.getPublications);
 
 module.exports = api;
