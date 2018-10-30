a63cc93b4bcded4c53cfdec488bd7dbde58930e0
Andres Morales <andmoraho@gmail.com>
2018-10-30 11:23:51 -0500

    Added CORS middleware

--- app.js
+++ app.js
@@ -13,6 +13,15 @@ const messageRoutes = require('./routes/message');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+//cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, x-auth, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 //routes
 app.use('/api', userRoutes);
 app.use('/api', followRoutes);
