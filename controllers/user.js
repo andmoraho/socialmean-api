9157d62c23ae56a734d47d7ac344b2dacece61c0
Andres Morales <andmoraho@gmail.com>
2018-11-27 11:10:59 -0500

    Removed expiration from jwt

--- controllers/user.js
+++ controllers/user.js
@@ -9,281 +9,301 @@ var { Follow } = require('../models/follow');
 var { Publication } = require('../models/publication');
 
 // POST /register
-var saveUser = async(req, res) => {
-    try {
-        const body = _.pick(req.body, ['name', 'surname', 'nick', 'email', 'password', 'role', 'image']);
-        var user = new User(body);
+var saveUser = async (req, res) => {
+  try {
+    const body = _.pick(req.body, [
+      'name',
+      'surname',
+      'nick',
+      'email',
+      'password',
+      'role',
+      'image'
+    ]);
+    var user = new User(body);
 
-        await user.save();
-        user = user.toObject();
-        delete user.password;
-        delete user.tokens;
+    await user.save();
+    user = user.toObject();
+    delete user.password;
+    delete user.tokens;
 
-        res.status(200).send(user);
-    } catch (error) {
-
-        res.status(400).send({
-            message: error.message
-        });
-    }
+    res.status(200).send(user);
+  } catch (error) {
+    res.status(400).send({
+      message: error.message
+    });
+  }
 };
 
 // POST /login
-var loginUser = async(req, res) => {
-    try {
-        const body = _.pick(req.body, ['email', 'password']);
-        var user = await User.findByCredentials(body.email, body.password);
-        const token = await user.generateAuthToken();
-        user = user.toObject();
-        delete user.password;
-        delete user.tokens;
-        delete user.__v;
+var loginUser = async (req, res) => {
+  try {
+    const body = _.pick(req.body, ['email', 'password']);
+    var user = await User.findByCredentials(body.email, body.password);
+    const token = await user.generateAuthToken();
+    user = user.toObject();
+    delete user.password;
+    delete user.tokens;
+    delete user.__v;
 
-        res.header('x-auth', token).status(200).send(user);
-    } catch (error) {
-        res.status(404).send();
-    }
+    res
+      .header('x-auth', token)
+      .status(200)
+      .send(user);
+  } catch (error) {
+    res.status(404).send();
+  }
 };
 
 // DELETE / logout(authenticated)
-var logoutUser = async(req, res) => {
-    try {
+var logoutUser = async (req, res) => {
+  try {
+    var userLogout = await User.findOneAndUpdate(
+      {
+        _id: req.user._id
+      },
+      { $set: { tokens: [] } },
+      { new: false }
+    );
 
-        var userLogout = await User.findOneAndUpdate({
-            _id: req.user._id
-        }, { $set: { tokens: [] } }, { new: false });
-
-        res.status(200).send();
-
-    } catch (error) {
-        res.status(400).send({
-            message: error.message
-        });
-    }
+    res.status(200).send();
+  } catch (error) {
+    res.status(400).send({
+      message: error.message
+    });
+  }
 };
-
 
 // GET /user/me (authenticated)
 var getMe = (req, res) => {
-    res.status(200).send(req.user);
+  res.status(200).send(req.user);
 };
 
 // GET /user/:id (authenticated)
-var getUser = async(req, res) => {
-    try {
-        const userId = req.params.id;
+var getUser = async (req, res) => {
+  try {
+    const userId = req.params.id;
 
-        if (!ObjectID.isValid(userId)) {
-            throw new Error('Id not valid.');
-        }
-
-        var user = await User.findById(userId).select({ '__v': 0, 'password': 0, 'tokens': 0 });
-
-        const following = await Follow.find({
-            _user: req.user._id
-        }).select({ '__v': 0, '_user': 0, '_id': 0 });
-
-        const followedMe = await Follow.find({
-            _followed: req.user._id
-        }).select({ '__v': 0, '_followed': 0, '_id': 0 });
-
-        res.status(200).send({
-            user,
-            following,
-            followedMe
-        });
-    } catch (error) {
-        res.status(404).send({ message: error.message });
+    if (!ObjectID.isValid(userId)) {
+      throw new Error('Id not valid.');
     }
+
+    var user = await User.findById(userId).select({
+      __v: 0,
+      password: 0,
+      tokens: 0
+    });
+
+    const following = await Follow.find({
+      _user: req.user._id
+    }).select({ __v: 0, _user: 0, _id: 0 });
+
+    const followedMe = await Follow.find({
+      _followed: req.user._id
+    }).select({ __v: 0, _followed: 0, _id: 0 });
+
+    res.status(200).send({
+      user,
+      following,
+      followedMe
+    });
+  } catch (error) {
+    res.status(404).send({ message: error.message });
+  }
 };
 
 // GET /users/page? (authenticated)
 
-var getUsers = async(req, res) => {
-    try {
-        var page = req.params.page || 1
-        var itemsPerPage = 1;
-        var totalUsers = await User.countDocuments({});
+var getUsers = async (req, res) => {
+  try {
+    var page = req.params.page || 1;
+    var itemsPerPage = 1;
+    var totalUsers = await User.countDocuments({});
 
-        var usersFiltered = await User.find({})
-            .select({ '__v': 0, 'role': 0, 'password': 0, 'tokens': 0 })
-            .skip((itemsPerPage * page) - itemsPerPage)
-            .limit(itemsPerPage);
+    var usersFiltered = await User.find({})
+      .select({ __v: 0, role: 0, password: 0, tokens: 0 })
+      .skip(itemsPerPage * page - itemsPerPage)
+      .limit(itemsPerPage);
 
-        const following = await Follow.find({
-            _user: req.user._id
-        }).select({ '__v': 0, '_user': 0, '_id': 0 });
+    const following = await Follow.find({
+      _user: req.user._id
+    }).select({ __v: 0, _user: 0, _id: 0 });
 
-        const followedMe = await Follow.find({
-            _followed: req.user._id
-        }).select({ '__v': 0, '_followed': 0, '_id': 0 });
+    const followedMe = await Follow.find({
+      _followed: req.user._id
+    }).select({ __v: 0, _followed: 0, _id: 0 });
 
-        res.status(200).send({
-            usersFiltered,
-            following,
-            followedMe,
-            total: totalUsers,
-            pages: Math.ceil(totalUsers / itemsPerPage),
-            currentPage: page
-        });
-
-    } catch (error) {
-        res.status(404).send({ message: error.message });
-    }
+    res.status(200).send({
+      usersFiltered,
+      following,
+      followedMe,
+      total: totalUsers,
+      pages: Math.ceil(totalUsers / itemsPerPage),
+      currentPage: page
+    });
+  } catch (error) {
+    res.status(404).send({ message: error.message });
+  }
 };
 
 // GET /user/counters/:id? (authenticated)
-var getCounters = async(req, res) => {
-    try {
-        const userId = req.params.id || req.user._id;
+var getCounters = async (req, res) => {
+  try {
+    const userId = req.params.id || req.user._id;
 
-        const countFollowing = await Follow.countDocuments({
-            _user: userId
-        }).select({ '__v': 0, '_user': 0, '_id': 0 });
+    const countFollowing = await Follow.countDocuments({
+      _user: userId
+    }).select({ __v: 0, _user: 0, _id: 0 });
 
-        const countFollowedMe = await Follow.countDocuments({
-            _followed: userId
-        }).select({ '__v': 0, '_followed': 0, '_id': 0 });
+    const countFollowedMe = await Follow.countDocuments({
+      _followed: userId
+    }).select({ __v: 0, _followed: 0, _id: 0 });
 
-        const countPublications = await Publication.countDocuments({
-            _user: userId
-        }).select({ '__v': 0, '_followed': 0, '_id': 0 });
+    const countPublications = await Publication.countDocuments({
+      _user: userId
+    }).select({ __v: 0, _followed: 0, _id: 0 });
 
-        res.status(200).send({
-            countFollowing,
-            countFollowedMe,
-            countPublications
-        });
-
-    } catch (error) {
-        res.status(404).send({ message: error.message });
-    }
-
+    res.status(200).send({
+      countFollowing,
+      countFollowedMe,
+      countPublications
+    });
+  } catch (error) {
+    res.status(404).send({ message: error.message });
+  }
 };
 
 // PUT /user/:id (authenticated)
-var updateUser = async(req, res) => {
-    try {
-        const id = req.params.id;
-        const body = _.pick(req.body, ['name', 'surname', 'nick', 'email', 'image']);
+var updateUser = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const body = _.pick(req.user, [
+      'name',
+      'surname',
+      'nick',
+      'email',
+      'image'
+    ]);
 
-        if (!ObjectID.isValid(id) || id != req.user._id) {
-            throw new Error('Id not valid.');
-        }
-        var userUpdated = await User.findOneAndUpdate({
-                _id: id
-            }, { $set: body }, { new: true })
-            .select({ '__v': 0, 'password': 0, 'tokens': 0 });
+    console.log(body);
 
-        res.status(200).send({ userUpdated });
-    } catch (error) {
-        res.status(404).send({
-            message: 'Unable to update user.'
-        });
+    if (!ObjectID.isValid(id) || id != req.user._id) {
+      throw new Error('Id not valid.');
     }
 
+    var userUpdated = await User.findOneAndUpdate(
+      {
+        _id: id
+      },
+      { $set: body },
+      { new: true }
+    ).select({ __v: 0, password: 0, tokens: 0 });
+
+    res.status(200).send({ userUpdated });
+  } catch (error) {
+    res.status(404).send({
+      message: 'Unable to update user.'
+    });
+  }
 };
 
 // POST /user/image/:id (authenticated)
-var uploadImage = async(req, res) => {
-    const id = req.params.id;
-    const allowed_extensions = {
-        png: 'png',
-        jpg: 'jpg',
-        jpeg: 'jpeg',
-        gif: 'gif'
-    };
+var uploadImage = async (req, res) => {
+  const id = req.params.id;
+  const allowed_extensions = {
+    png: 'png',
+    jpg: 'jpg',
+    jpeg: 'jpeg',
+    gif: 'gif'
+  };
 
-    try {
+  try {
+    if (req.files) {
+      const file_path = req.files.image.path;
+      const file_params = file_path.split('\\');
+      const file_name = file_params[2];
+      const file_name_split = file_name.split('.');
+      const file_extension = file_name_split[1];
 
-        if (req.files) {
+      if (!ObjectID.isValid(id) || id != req.user._id) {
+        return removeFileUploaded(res, file_path, 'User Id not valid.');
+      }
 
-            const file_path = req.files.image.path;
-            const file_params = file_path.split('\\');
-            const file_name = file_params[2];
-            const file_name_split = file_name.split('\.');
-            const file_extension = file_name_split[1];
+      if (validator.isIn(file_extension, allowed_extensions)) {
+        const prevUser = await User.findById(id);
+        const prevImagePath = `./uploads/users/${prevUser.image}`;
 
-            if (!ObjectID.isValid(id) || id != req.user._id) {
-                return removeFileUploaded(res, file_path, 'User Id not valid.');
-            }
+        var userUpdated = await User.findOneAndUpdate(
+          {
+            _id: id
+          },
+          { $set: { image: file_name } },
+          { new: true }
+        ).select({ __v: 0, role: 0, password: 0, tokens: 0 });
 
-            if (validator.isIn(file_extension, allowed_extensions)) {
-
-
-                const prevUser = await User.findById(id);
-                const prevImagePath = `./uploads/users/${prevUser.image}`;
-
-                var userUpdated = await User.findOneAndUpdate({
-                        _id: id
-                    }, { $set: { image: file_name } }, { new: true })
-                    .select({ '__v': 0, 'role': 0, 'password': 0, 'tokens': 0 });
-
-                fs.exists(prevImagePath, (exists) => {
-                    if (exists) {
-                        fs.unlink(prevImagePath, (error) => {
-                            if (error) {
-                                // Do nothing
-                            };
-                        });
-                    }
-
-                });
-
-                res.status(200).send({ userUpdated });
-
-
-            } else {
-                return removeFileUploaded(res, file_path.toString(), 'Image extension not valid.');
-            }
-
-        } else {
-            return res.status(400).send({
-                message: 'Image is required.'
+        fs.exists(prevImagePath, exists => {
+          if (exists) {
+            fs.unlink(prevImagePath, error => {
+              if (error) {
+                // Do nothing
+              }
             });
-        }
-    } catch (error) {
-        res.status(404).send({
-            message: 'Unable to update user image.'
+          }
         });
-    }
 
+        res.status(200).send({ userUpdated });
+      } else {
+        return removeFileUploaded(
+          res,
+          file_path.toString(),
+          'Image extension not valid.'
+        );
+      }
+    } else {
+      return res.status(400).send({
+        message: 'Image is required.'
+      });
+    }
+  } catch (error) {
+    res.status(404).send({
+      message: 'Unable to update user image.'
+    });
+  }
 };
 
 var removeFileUploaded = (res, file_path, message) => {
-    fs.unlink(file_path, (error) => {
-        return res.status(400).send({
-            message
-        });
+  fs.unlink(file_path, error => {
+    return res.status(400).send({
+      message
     });
+  });
 };
 
 //GET /user/image/:imageFile (authenticated)
 var getImageUser = (req, res) => {
-    const imageUser = req.params.imageFile;
-    const imagePath = `./uploads/users/${imageUser}`;
+  const imageUser = req.params.imageFile;
+  const imagePath = `./uploads/users/${imageUser}`;
 
-    fs.exists(imagePath, (exists) => {
-        if (exists) {
-            res.sendFile(path.resolve(imagePath));
-        } else {
-            return res.status(404).send({
-                message: 'Unable to find image.'
-            });
-        }
-    });
-
+  fs.exists(imagePath, exists => {
+    if (exists) {
+      res.sendFile(path.resolve(imagePath));
+    } else {
+      return res.status(404).send({
+        message: 'Unable to find image.'
+      });
+    }
+  });
 };
 
 module.exports = {
-    saveUser,
-    loginUser,
-    logoutUser,
-    getMe,
-    getUser,
-    getUsers,
-    getCounters,
-    updateUser,
-    uploadImage,
-    getImageUser
+  saveUser,
+  loginUser,
+  logoutUser,
+  getMe,
+  getUser,
+  getUsers,
+  getCounters,
+  updateUser,
+  uploadImage,
+  getImageUser
 };
--- models/user.js
+++ models/user.js
@@ -7,150 +7,155 @@ const moment = require('moment');
 var { ObjectID } = require('mongodb');
 
 var UserSchema = new mongoose.Schema({
-    name: {
+  name: {
+    type: String,
+    required: true,
+    minlength: [1, 'Name is shorter than the minimum allowed length']
+  },
+  surname: {
+    type: String,
+    required: true,
+    minlength: [1, 'Surname is shorter than the minimum allowed length']
+  },
+  nick: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: [2, 'Nick is shorter than the minimum allowed length']
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email'
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: [6, 'Password is shorter than the minimum allowed length']
+  },
+  role: {
+    type: String
+  },
+  image: {
+    type: String
+  },
+  tokens: [
+    {
+      access: {
         type: String,
-        required: true,
-        minlength: [1, 'Name is shorter than the minimum allowed length']
-    },
-    surname: {
+        required: true
+      },
+      token: {
         type: String,
-        required: true,
-        minlength: [1, 'Surname is shorter than the minimum allowed length']
-    },
-    nick: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: [2, 'Nick is shorter than the minimum allowed length']
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 1,
-        unique: true,
-        validate: {
-            validator: validator.isEmail,
-            message: '{VALUE} is not a valid email'
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: [6, 'Password is shorter than the minimum allowed length']
-    },
-    role: {
-        type: String
-    },
-    image: {
-        type: String
-    },
-    tokens: [{
-        access: {
-            type: String,
-            required: true
-        },
-        token: {
-            type: String,
-            required: true
-        }
-    }]
+        required: true
+      }
+    }
+  ]
 });
 
 UserSchema.methods.generateAuthToken = function() {
-    var user = this;
-    var access = 'auth';
-    var token = jwt.sign({
-            _id: user._id.toHexString(),
-            role: user.role,
-            iat: moment().unix(),
-            exp: moment().add(60, 'minutes').unix(),
-            access
-        },
-        process.env.JWT_SECRET).toString();
+  var user = this;
+  var access = 'auth';
+  var token = jwt
+    .sign(
+      {
+        _id: user._id.toHexString(),
+        role: user.role,
+        iat: moment().unix(),
+        access
+      },
+      process.env.JWT_SECRET
+    )
+    .toString();
 
-    user.tokens = user.tokens.concat([{ access, token }]);
+  user.tokens = user.tokens.concat([{ access, token }]);
 
-    return user.save().then(() => {
-        return token;
-    });
+  return user.save().then(() => {
+    return token;
+  });
 };
 
 UserSchema.methods.removeToken = function(token) {
-    var user = this;
+  var user = this;
 
-    return user.updateOne({
-        $pull: {
-            tokens: {
-                token
-            }
-        }
-    });
+  return user.updateOne({
+    $pull: {
+      tokens: {
+        token
+      }
+    }
+  });
 };
 
 UserSchema.statics.findByToken = function(token) {
-    var user = this;
-    var decoded;
+  var user = this;
+  var decoded;
 
-    try {
-        decoded = jwt.verify(token, process.env.JWT_SECRET, { maxAge: '60m' });
-    } catch (error) {
-        return Promise.reject().then(function() {
-            throw new Error();
-        });
-    }
-
-    user.findOne({
-        '_id': decoded._id
-    }, (error, result) => {
-        if (error) {
-            return Promise.reject().then(function() {
-                throw new Error(error);
-            });
-        }
-
-        return result;
+  try {
+    decoded = jwt.verify(token, process.env.JWT_SECRET, { maxAge: '60m' });
+  } catch (error) {
+    return Promise.reject().then(function() {
+      throw new Error();
     });
+  }
 
+  user.findOne(
+    {
+      _id: decoded._id
+    },
+    (error, result) => {
+      if (error) {
+        return Promise.reject().then(function() {
+          throw new Error(error);
+        });
+      }
 
+      return result;
+    }
+  );
 };
 
 UserSchema.statics.findByCredentials = async function(email, password) {
-    var user = this;
+  var user = this;
 
-    return user.findOne({ email }).then((userFind) => {
-        if (!userFind) {
-            return Promise.reject().then(function() {
-                throw new Error();
-            });
+  return user.findOne({ email }).then(userFind => {
+    if (!userFind) {
+      return Promise.reject().then(function() {
+        throw new Error();
+      });
+    }
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, userFind.password, (error, result) => {
+        if (result) {
+          resolve(userFind);
+        } else {
+          reject(new Error(error));
         }
-
-        return new Promise((resolve, reject) => {
-            bcrypt.compare(password, userFind.password, (error, result) => {
-                if (result) {
-                    resolve(userFind);
-                } else {
-                    reject(new Error(error));
-                }
-            });
-        });
+      });
     });
-
+  });
 };
 
 UserSchema.pre('save', function(next) {
-    var user = this;
+  var user = this;
 
-    if (user.isModified('password')) {
-        bcrypt.genSalt(10, (err, salt) => {
-            bcrypt.hash(user.password, salt, null, (err, hash) => {
-                user.password = hash;
-                next();
-            });
-        });
-    } else {
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, null, (err, hash) => {
+        user.password = hash;
         next();
-    }
+      });
+    });
+  } else {
+    next();
+  }
 });
 
 var User = mongoose.model('User', UserSchema);
